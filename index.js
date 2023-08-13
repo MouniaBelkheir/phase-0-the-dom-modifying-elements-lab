7ea5eb548faac2ea72f3abf8e78546953d3c4a0b
mouniabelkheir <mouniabelkheir@outlook.com>
2023-08-13 21:29:04 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+    const mainNode = document.querySelector('#main');
+    mainNode.remove();
+  
+    const newHeader = document.createElement('h1');
+  newHeader.id = 'victory';
+  newHeader.textContent = "YOUR-NAME is the champion";
+
+  const header = document.querySelector('header');
+  header.appendChild(newHeader);
